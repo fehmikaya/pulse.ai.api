f353505e1e8ef3990a6426075c18485fe43c91ff
Ozan <Ozan@Ozans-MacBook-Pro.local>
2019-08-06 21:59:00 +0300

    test change

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// this will be our data base's data structure 
+// this. will be our data base's data structure 
 const DataSchema = new Schema(
     {
         id: Number,
